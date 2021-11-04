ff6057daaecd542b772ec3297094166659206cfe
Ronal Castillo <castilloronal@hotmail.com>
2021-11-04 11:56:48 +0100

    SE colocaron las imagenes a los botones, se coloco la grilla para realizar las consultas y s, posicion frames

--- dvd.py
+++ dvd.py
@@ -3,12 +3,14 @@ from tkinter import *
 from tkinter import messagebox
 import mysql.connector as mysqlconn
 from mysql.connector.errors import ProgrammingError
+import sqlite3
+import tkinter.font as tkFont
+import tktabl
 
 # Se define el frame principal
 root = Tk()
 root.iconbitmap("../MalagaDVD/imagenes/python.ico")
 root.title("frmPrincipal")
-root.columnconfigure(2, weight=1)
 
 # Se obtiene el acho y el alto de la pantalla del sistema
 ancho = root.winfo_screenwidth()
@@ -16,14 +18,21 @@ alto = root.winfo_screenheight()
 
 # Creación del Frame donde se van a colocar todos los widgets
 frmPrincipal = Frame(root, width=ancho, height=alto)
-frmPrincipal.grid()
+frmPrincipal.config(bg='green', bd=15, width=1500, height=800, padx=10)
+# frmPrincipal.pack(fill=X)
+frmPrincipal.grid(column=1, row=0)
 
-# SE define la clase principal
+frmGrilla = Frame(frmPrincipal, width=1700, height=900)
+frmGrilla.config(bg='red', bd=12)
+frmGrilla.grid(column=1, row=0)
+# frmGrilla.pack(side=RIGHT)
+
+
+# Se define la clase principal
 
 
 class principal():
     # Función para centrar ventana
-
     def centrarVentana(ancho, alto):
         ancho_ventana = ancho
         alto_ventana = alto
@@ -34,12 +43,96 @@ class principal():
         root.geometry(posicion)
         root.resizable(0, 0)
 
+    # def creacionGrilla(col, fil):
+        # Creación de la grilla principal
+        #tblPrincipal = tktabl.Table(frmPrincipal)
+        # tblPrincipal.pack()
 
-# Instrucciones para establecer la barra de menu
+    def numColTablas(tabla):
+        try:
+            datos = []
+            mydb = mysqlconn.connect(
+                host="localhost",
+                user="root",
+                password="hola",
+                database="malagadvd"
+            )
+            mycursor = mydb.cursor()
+            sql = "SELECT COUNT(*) AS numColumnas FROM information_schema.columns WHERE table_name = %s"
+            mycursor.execute(sql, (tabla,))
+            datos = mycursor.fetchone()
+            mydb.commit()
+            mydb.close()
+
+            messagebox.showinfo("Contador columnas actores",
+                                "La consulta sql ha sido satisfactoria")
+        except mysqlconn.ProgrammingError as e:
+            messagebox.showwarning("Error Base de datos", e)
+
+    def numRowTablas(tabla):
+        try:
+            datos = []
+            mydb = mysqlconn.connect(
+                host="localhost",
+                user="root",
+                password="hola",
+                database="malagadvd"
+            )
+            mycursor = mydb.cursor()
+            sql = "SELECT COUNT(*) FROM " + tabla
+            mycursor.execute(sql)
+            datos = mycursor.fetchone()
+            mydb.commit()
+            mydb.close()
+
+            messagebox.showinfo("Contador columnas actores",
+                                "La consulta sql ha sido satisfactoria")
+        except mysqlconn.ProgrammingError as e:
+            messagebox.showwarning("Error Base de datos", e)
+
+    def mostrarTablaGrilla(tabla):
+        try:
+            datos = []
+            mydb = mysqlconn.connect(
+                host="localhost",
+                user="root",
+                password="hola",
+                database="malagadvd"
+            )
+            mycursor = mydb.cursor()
+            sql = "SELECT * FROM " + tabla
+            mycursor.execute(sql)
+            datos = mycursor.fetchall()
+
+            mydb.commit()
+            mydb.close()
+
+            totalRows = len(datos)
+            totalCols = len(datos[0])
+            print(totalRows)
+            print(totalCols)
+
+            encabezado = ["Id Actor", "Nombre Actor", "Nacionalidad", "Sexo"]
+            grilla = tktabl.Table(
+                frmGrilla, col=totalCols, row=totalRows, headers=encabezado)
+            # code for creating table
+            for i in range(totalRows-1):
+                for j in range(totalCols):
+                    grilla = Entry(frmGrilla, width=20,
+                                   fg='blue', font=('tahoma', 8, 'bold'))
+                    grilla.grid(row=i, column=j)
+                    grilla.insert(END, datos[i][j])
+            # messagebox.showinfo("Listado Tabla Actores",
+                    # "Consulta de actores realizada con éxito")
+        except mysqlconn.ProgrammingError as e:
+            messagebox.showwarning("Error Base de datos", e)
+
+    # Instrucciones para establecer la barra de menu
 barraMenu = Menu(root)
 root.config(menu=barraMenu)
 
-mnubarClientes = Menu(barraMenu, tearoff=0)
+tipoLetra = ('Tahoma', 12, 'bold')
+mnubarClientes = Menu(barraMenu, tearoff=0, font=tipoLetra)
 barraMenu.add_cascade(label="Clientes", menu=mnubarClientes)
 mnubarClientes.add_command(label="Formulario Clientes")
 
@@ -59,21 +152,28 @@ mnubarEjemplares = Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="Ejemplares", menu=mnubarClientes)
 mnubarEjemplares.add_command(label="Formulario Ejemplares")
 
-# Creacion de los Botones CRUD
+# Creacion de los Botones
 frmBotones = Frame(frmPrincipal)
 frmBotones.configure(bg="light blue")
-frmBotones.pack(padx=10, pady=10)
+frmBotones.grid(sticky=W+N+S, column=0, row=0, padx=20)
+#frmBotones.pack(padx=10, pady=10, side='left')
 
-btnClientes = Button(frmBotones, text="Clientes", width=12, height=5)
+imgClientes = PhotoImage(file=r"../MalagaDVD/imagenes/clientes.png")
+btnClientes = Button(frmBotones, image=imgClientes, text="Clientes", compound="center",
+                     width=100, height=100, command=lambda: principal.numColActores("clientes"))
 btnClientes.grid(column=0, row=0, padx=20, pady=20)
 
-btnActores = Button(frmBotones, text="Actores", width=12, height=5)
+btnActores = Button(frmBotones, text="Actores", width=12, height=5,
+                    command=lambda: principal.mostrarTablaGrilla("actores"))
 btnActores.grid(column=0, row=1, padx=20, pady=20)
 
-btnProtagonistas = Button(frmBotones, text="Protagonistas", width=12, height=5)
+btnProtagonistas = Button(frmBotones, text="Protagonistas", width=12,
+                          height=5, command=lambda: principal.numRowTablas("clientes"))
 btnProtagonistas.grid(column=0, row=2, padx=20, pady=20)
 
-btnDirectores = Button(frmBotones, text="Directores", width=12, height=5)
+imgDirectores = PhotoImage(file=r"../MalagaDVD/imagenes/director.png")
+btnDirectores = Button(frmBotones, image=imgDirectores, text="Directores", compound="center",
+                       width=100, height=100, command=lambda: principal.mostrarTablaGrilla("actores"))
 btnDirectores.grid(column=0, row=3, padx=20, pady=20)
 
 btnEjemplares = Button(frmBotones, text="Ejemplares", width=12, height=5)
